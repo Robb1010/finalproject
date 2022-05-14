dda5a796b16d66cd57d8d8d582bf2e9d13fdf007
Robert Soldan <soldanrobertandrei@gmail.com>
2022-05-14 19:42:33 +0100

    Final version

---
+++ scrape_releases.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from time import sleep
+
+url = "https://api.github.com/repos/microsoft/vscode/releases"
+
+text = "123"
+count = 1
+data = []
+while len(text) > 2:
+
+    querystring = {f"page":{count},"per_page":"100"}
+
+    payload = ""
+    headers = {"Authorization": "Bearer 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"}
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    text = response.text
+    if len(text) > 2:
+        data.extend(json.loads(text))
+    print(f"Writing page {count}")
+    count += 1
+    sleep(0.5)
+
+with open('releases2.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
